7b5dfeba9d8fa4164122589278eaa7334edb13d0
Alsu Ibragimova <alsu.ibragimova@gmail.com>
2024-12-06 16:28:30 -0500

    modify tests and fixtures

--- __tests__/test1.js
+++ __tests__/test1.js
@@ -1,69 +1,36 @@
 import path from 'path';
 import { fileURLToPath } from 'url';
+import fs from 'fs';
 import diff from '../src/diff.js';
 
 const __filename = fileURLToPath(import.meta.url); // Получение текущего файла
 const __dirname = path.dirname(__filename); // Получение директории файла
+// не использую import.meta.dirname тк использую версию Node.js v18.20.4 ()
 
-const expect1 = `{
-  host : hexlet.io
-- timeout : 50
-+ timeout : 20
-- proxy : 123.234.53.22
-- follow : false
-+ verbose : true
-}`;
+let json1;
+let json2;
+let yml1;
+let yml2;
+let twoFilesStylish;
 
-const expect2 = `{
-  host : hexlet.io
-  timeout : 50
-  proxy : 123.234.53.22
-  follow : false
-}`;
-
-const expect3 = `{
-- host : hexlet.io
-- timeout : 50
-- proxy : 123.234.53.22
-- follow : false
-}`;
-
-const expect4 = `{
-+ host : hexlet.io
-+ timeout : 50
-+ proxy : 123.234.53.22
-+ follow : false
-}`;
-
-const expect5 = `{
-
-}`;
-
-const plainJson1 = path.join(__dirname, '../__fixtures__/plainFile1.json');
-const plainJson2 = path.join(__dirname, '../__fixtures__/plainFile2.json');
-const emptyJson = path.join(__dirname, '../__fixtures__/empty.json');
-const plainYml1 = path.join(__dirname, '../__fixtures__/plainFile1.yml');
-const plainYml2 = path.join(__dirname, '../__fixtures__/plainFile2.yml');
-const emptyYml = path.join(__dirname, '../__fixtures__/empty.yml');
-
-test('plain json files', () => {
-  expect(diff(plainJson1, plainJson2)).toEqual(expect1);
-  expect(diff(plainJson1, plainJson1)).toEqual(expect2);
-  expect(diff(plainJson1, emptyJson)).toEqual(expect3);
-  expect(diff(emptyJson, plainJson1)).toEqual(expect4);
+beforeAll(() => {
+  json1 = path.join(__dirname, '../__fixtures__/file1.json');
+  json2 = path.join(__dirname, '../__fixtures__/file2.json');
+  // const emptyJson = path.join(__dirname, '../__fixtures__/empty.json');
+  yml1 = path.join(__dirname, '../__fixtures__/file1.yml');
+  yml2 = path.join(__dirname, '../__fixtures__/file2.yml');
+  // const emptyYml = path.join(__dirname, '../__fixtures__/empty.yml');
+  twoFilesStylish = fs.readFileSync(path.join(__dirname, '../__fixtures__/twoFilesStylish.txt'), 'utf-8');
 });
 
-test('plain yml files', () => {
-  expect(diff(plainYml1, plainYml2)).toEqual(expect1);
-  expect(diff(plainYml1, plainYml1)).toEqual(expect2);
-  expect(diff(plainYml1, emptyYml)).toEqual(expect3);
-  expect(diff(emptyYml, plainYml1)).toEqual(expect4);
+test('two json files, default stylish', () => {
+  expect(diff(json1, json2)).toEqual(twoFilesStylish);
 });
 
-test('plain json and yml files', () => {
-  expect(diff(plainYml1, plainJson2)).toEqual(expect1);
-  expect(diff(plainYml1, plainJson1)).toEqual(expect2);
-  expect(diff(plainYml1, emptyJson)).toEqual(expect3);
-  expect(diff(emptyYml, plainJson1)).toEqual(expect4);
-  expect(diff(emptyYml, emptyJson)).toEqual(expect5);
+test('two yml files, default stylish', () => {
+  expect(diff(yml1, yml2)).toEqual(twoFilesStylish);
+});
+
+test('json and yml files, default stylish', () => {
+  expect(diff(yml1, json2)).toEqual(twoFilesStylish);
 });
