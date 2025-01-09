4da64042916e459a16e4065ffbf6e1172b4d5cd5
Alsu Ibragimova <alsu.ibragimova@gmail.com>
2025-01-09 14:54:43 -0500

    modify formatter.js for stylish format (major change), modify test1.js, diff.js (minor)

--- __tests__/test1.js
+++ __tests__/test1.js
@@ -1,43 +1,31 @@
 import path from 'path';
 import { fileURLToPath } from 'url';
-// import fs from 'fs';
+import fs from 'fs';
 import diff from '../src/diff.js';
-import myExpectedAST from '../__fixtures__/expected/myExpectedAST.js';
 
 const __filename = fileURLToPath(import.meta.url); // Получение текущего файла
 const __dirname = path.dirname(__filename); // Получение директории файла
 // не использую import.meta.dirname тк использую версию Node.js v18.20.4 ()
 
-// let json1;
-// let json2;
-// let yml1;
-// let yml2;
-// let twoFilesStylish;
-let myJson1;
-let myJson2;
-let myYml1;
-let myYml2;
+let json1;
+let json2;
+let yml1;
+let yml2;
+let twoFilesStylish;
 
 beforeAll(() => {
-  myJson1 = path.join(__dirname, '../__fixtures__/myFile1.json');
-  myJson2 = path.join(__dirname, '../__fixtures__/myFile2.json');
-  myYml1 = path.join(__dirname, '../__fixtures__/myFile1.yml');
-  myYml2 = path.join(__dirname, '../__fixtures__/myFile2.yml');
-  // json1 = path.join(__dirname, '../__fixtures__/file1.json');
-  // json2 = path.join(__dirname, '../__fixtures__/file2.json');
-  // yml1 = path.join(__dirname, '../__fixtures__/file1.yml');
-  // yml2 = path.join(__dirname, '../__fixtures__/file2.yml');
+  json1 = path.join(__dirname, '../__fixtures__/file1.json');
+  json2 = path.join(__dirname, '../__fixtures__/file2.json');
+  yml1 = path.join(__dirname, '../__fixtures__/file1.yml');
+  yml2 = path.join(__dirname, '../__fixtures__/file2.yml');
   // eslint-disable-next-line max-len
-  // twoFilesStylish = fs.readFileSync(path.join(__dirname, '../__fixtures__/twoFilesStylish.txt'), 'utf-8');
+  twoFilesStylish = fs.readFileSync(path.join(__dirname, '../__fixtures__/expected/twoFilesStylish.txt'), 'utf-8');
 });
 
-test('my json files comparison AST', () => {
-  // const result = diff(myJson1, myJson2);
-  // console.log('Result:', JSON.stringify(result, null, 2));
-  // console.log('Expected:', JSON.stringify(myExpectedAST, null, 2));
-  expect(diff(myJson1, myJson2)).toEqual(myExpectedAST);
+test('json files comparison AST', () => {
+  expect(diff(json1, json2)).toEqual(twoFilesStylish);
 });
 
-test('my yml files comparison AST', () => {
-  expect(diff(myYml1, myYml2)).toEqual(myExpectedAST);
+test('yml files comparison AST', () => {
+  expect(diff(yml1, yml2)).toEqual(twoFilesStylish);
 });
--- src/diff.js
+++ src/diff.js
@@ -1,10 +1,12 @@
 import prepareObjects from './prepareObjects.js';
 import prepareAST from './prepareAST.js';
+import formatter from './formatter.js';
 
 const diff = (filepath1, filepath2) => {
   const { object1, object2 } = prepareObjects(filepath1, filepath2);
   const ast = prepareAST(object1, object2);
-  return ast;
+  const result = formatter(ast);
+  return result;
 };
 
 export default diff;
