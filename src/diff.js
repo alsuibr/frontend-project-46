c5a69541b4d22a987c0d948daad4d30b2bb4576c
Alsu Ibragimova <alsu.ibragimova@gmail.com>
2025-01-10 14:35:22 -0500

    fix according to linter

--- src/diff.js
+++ src/diff.js
@@ -2,7 +2,6 @@ import prepareObjects from './prepareObjects.js';
 import prepareAST from './prepareAST.js';
 import formatter from './formatter.js';
 
-
 const diff = (filepath1, filepath2, formatName = 'stylish') => {
   const { object1, object2 } = prepareObjects(filepath1, filepath2);
   const ast = prepareAST(object1, object2);
